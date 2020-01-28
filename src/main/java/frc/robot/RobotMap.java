5df141f8119b283b6f65e040bd32d459318b8eb1
Tommy <tommytrident@outlook.com>
2020-01-28 17:33:56 -0500

    DriveTrain

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,5 +1,7 @@
 package frc.robot;
-
+/**
+ * Robot Map contains the Robot Motor ID's for both sides of the DriveTrain.
+ */
 public class RobotMap {
     public static final int RIGHT_MASTER_ID = -1;
     public static final int RIGHT_SLAVE_ID = -1;
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,24 +1,54 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
 
 public class DriveTrain extends SubsystemBase {
+
+  private WPI_TalonSRX lMaster;
+  private WPI_TalonSRX lSlave;
+  private WPI_TalonSRX rMaster;
+  private WPI_TalonSRX rSlave;
+  
+  private DifferentialDrive drive;
+  
   /**
    * Creates a new DriveTrain.
    */
   public DriveTrain() {
+    lMaster = new WPI_TalonSRX(RobotMap.LEFT_MASTER_ID);
+    lSlave = new WPI_TalonSRX(RobotMap.LEFT_SLAVE_ID);
 
-  }
+    rMaster = new WPI_TalonSRX(RobotMap.RIGHT_MASTER_ID);
+    rSlave = new WPI_TalonSRX(RobotMap.RIGHT_SLAVE_ID);
 
+    lMaster.configFactoryDefault();
+    lSlave.configFactoryDefault();
+
+    rMaster.configFactoryDefault();
+    rSlave.configFactoryDefault();
+    
+    lSlave.set(ControlMode.Follower,RobotMap.LEFT_MASTER_ID);
+    rSlave.set(ControlMode.Follower,RobotMap.RIGHT_MASTER_ID);
+    
+    drive = new DifferentialDrive(new SpeedControllerGroup(lMaster),new SpeedControllerGroup(rMaster));
+
+    
+    }
+    public void move(double leftSpeed, double rightSpeed){
+      drive.tankDrive(leftSpeed, rightSpeed);
+    }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+  public void stop(){
+    lMaster.stopMotor();
+    rMaster.stopMotor();
+  }
+  
 }
