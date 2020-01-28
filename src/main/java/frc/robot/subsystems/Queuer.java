a93241640aaf5c7725ca2e5096c0257091bd357c
Jack <jack@Jacks-MBP-3.nicholsschool.org>
2020-01-28 17:34:46 -0500

    added configFactoryDefault() method to each motor in queuer

--- src/main/java/frc/robot/subsystems/Queuer.java
+++ src/main/java/frc/robot/subsystems/Queuer.java
@@ -6,16 +6,22 @@ import frc.robot.RobotMap;
 
 public class Queuer
 {
-    private WPI_TalonSRX lockTwo;
-    private WPI_TalonSRX lockThree;
-    private WPI_TalonSRX lockFour;
-    private WPI_TalonSRX lockFive;
+    private WPI_TalonSRX lock2;
+    private WPI_TalonSRX lock3;
+    private WPI_TalonSRX lock4;
+    private WPI_TalonSRX lock5;
 
     public Queuer()
     {
-        lockTwo = new WPI_TalonSRX(RobotMap.LOCK_TWO_MOTOR_ID);
-        lockThree = new WPI_TalonSRX(RobotMap.LOCK_THREE_MOTOR_ID);
-        lockFour = new WPI_TalonSRX(RobotMap.LOCK_FOUR_MOTOR_ID);
-        lockFive = new WPI_TalonSRX(RobotMap.LOCK_FIVE_MOTOR_ID);
+        lock2 = new WPI_TalonSRX(RobotMap.LOCK_TWO_MOTOR_ID);
+        lock3 = new WPI_TalonSRX(RobotMap.LOCK_THREE_MOTOR_ID);
+        lock4 = new WPI_TalonSRX(RobotMap.LOCK_FOUR_MOTOR_ID);
+        lock5 = new WPI_TalonSRX(RobotMap.LOCK_FIVE_MOTOR_ID);
+
+        lock2.configFactoryDefault();
+        lock3.configFactoryDefault();
+        lock4.configFactoryDefault();
+        lock5.configFactoryDefault();
+        
     }
 }
