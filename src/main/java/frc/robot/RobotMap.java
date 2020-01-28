7aaa18251fd7f4e2fbf39ca1c18cc676c66ada5e
Jack <jack@Jacks-MBP-3.nicholsschool.org>
2020-01-28 16:56:54 -0500

    made queuer subsystem and motors in robot map

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,5 +1,10 @@
 package frc.robot;
 
 public class RobotMap {
+
+    public static final int LOCK_TWO_MOTOR_ID = -1;
+    public static final int LOCK_THREE_MOTOR_ID = -1;
+    public static final int LOCK_FOUR_MOTOR_ID = -1;
+    public static final int LOCK_FIVE_MOTOR_ID = -1;
     
 }
