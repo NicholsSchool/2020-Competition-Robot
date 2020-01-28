1774122a4b6340b63b1d9d97b970312249715953
Tommy <tommytrident@outlook.com>
2020-01-28 16:57:03 -0500

    Robot Map ID's

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,5 +1,9 @@
 package frc.robot;
 
 public class RobotMap {
-    
+    public static final int RIGHT_MASTER_ID = -1;
+    public static final int RIGHT_SLAVE_ID = -1;
+
+    public static final int LEFT_MASTER_ID = -1;
+    public static final int LEFT_SLAVE_ID = -1;
 }
