47723fab69f9a955969eb4fd94f66cb906edfc55
ZachTB1234 <zboeck@verizon.net>
2020-01-30 17:11:10 -0500

    added smartdashboard for dart eletrical current

--- src/main/java/frc/robot/subsystems/Dart.java
+++ src/main/java/frc/robot/subsystems/Dart.java
@@ -9,6 +9,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
@@ -39,4 +40,8 @@ public class Dart extends SubsystemBase {
 
   }
 
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("Dart Eletrical Current:", dart.getStatorCurrent());
+  }
 }
