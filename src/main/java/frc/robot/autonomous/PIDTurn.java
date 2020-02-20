a9e367e9907774b3b0dc24c07c3b96420df095cb
Tommy <tommytrident@outlook.com>
2020-02-20 17:39:48 -0500

    Fixed PID

--- src/main/java/frc/robot/autonomous/PIDTurn.java
+++ src/main/java/frc/robot/autonomous/PIDTurn.java
@@ -9,7 +9,9 @@ package frc.robot.autonomous;
 
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.PIDCommand;
+import frc.robot.Constants;
 import frc.robot.RobotContainer;
+
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
@@ -20,20 +22,20 @@ public class PIDTurn extends PIDCommand {
   public PIDTurn(double agl) {
     super(
         // The controller that the command will use
-        new PIDController(0.005, 0, 0),
+        new PIDController(0.015, 0, 0.0015),
         // This should return the measurement
         () -> RobotContainer.navX.getAngle(),
         // This should return the setpoint (can also be a constant)
         () -> agl,
         // This uses the output
         output -> {
-          RobotContainer.driveTrain.move(output, -output);
-
+          output += Math.copySign(Constants.VISION_kF, output);
+          RobotContainer.driveTrain.move(-output, output);
 
           // Use the output here
         });
-        addRequirements(RobotContainer.driveTrain);
-        getController().setTolerance(5);
+    addRequirements(RobotContainer.driveTrain);
+    getController().setTolerance(5);
     // Use addRequirements() here to declare subsystem dependencies.
     // Configure additional PID options by calling `getController` here.
   }
